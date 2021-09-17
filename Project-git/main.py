ba7ca9681443125b5d0b5df6262ba26c047b22ed
WithoutKitu <69809585+WithoutKitu@users.noreply.github.com>
2021-09-17 17:04:35 +0200

    adding program and device list (interacitve)

--- Project-git/main.py
+++ Project-git/main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+file = open(".\\devices.txt", "a")
+
+while True:
+	device = input("Podaj nazwe urzadzenia: ")
+	if device == "exit":
+		break
+	else:
+		file.write(device + "\n")
+
